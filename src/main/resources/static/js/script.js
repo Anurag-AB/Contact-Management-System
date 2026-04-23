c06dd4b5800905b79f086a19ac919e4d10c86025
Anurag-AB <anuragbehera929@gmail.com>
2026-04-23 13:17:55 +0530

    Added docker file

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -22,3 +22,35 @@ function confirmDelete(id) {
     }
 }
 
+const search = () => {
+
+	let query = $("#search-input").val().trim();
+
+	if (query == '') {
+		$(".search-result").hide();
+	} else {
+
+		let url = `http://localhost:8083/search/${query}`;
+
+		fetch(url)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+
+				let text = `<div class='list-group'>`;
+
+				data.forEach((contact) => {
+					text += `<a href='/user/${contact.cId}/contact' class="list-group-item list-group-item-action">${contact.name}</a>`;
+				});
+
+				text += `</div>`;
+
+				$(".search-result").html(text);
+				$(".search-result").show();
+			});
+	}
+}
+
+
